76a4caf5ea0527c755c619211716c221f2cecb25
Minister <mundestephane13@gmail.com>
2023-06-24 07:20:11 +0100

    ...

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -2,47 +2,23 @@
   <div class="Input">
     <div class="Input-Wrapper">
       <slot name="leftIcon">
-        <Icon
-          v-if="leftIcon"
-          :class="['Input-LeftIcon', classes?.leftIcon]"
-          :path="leftIcon"
-        />
+        <Icon v-if="leftIcon" :class="['Input-LeftIcon', classes?.leftIcon]" :path="leftIcon" />
       </slot>
 
-      <input
-        :type="type"
-        :placeholder="placeholder"
-        :value="modelValue"
-        :class="[
-          'focus:outline-none h-full grow bg-transparent InputInut',
-          classes?.input
-        ]"
-        :required="required"
-        @input="
+      <input :type="type" :placeholder="placeholder" :value="modelValue" :class="['Input-Input', classes?.input]"
+        :required="required" @input="
           $emit('update:model-value', ($event.target as HTMLInputElement).value)
-        "
-      />
+          " />
 
       <slot name="rightIcon">
-        <Icon
-          v-if="rightIcon || props.type == 'password'"
-          :class="[
-            'h-4 aspect-square cursor-pointer text-primary InputRightIcon',
-            classes?.rightIcon
-          ]"
-          :path="
-            rightIcon ?? type == 'password' ? mdiEyeOutline : mdiEyeOffOutline
-          "
-          @click="type = type == 'password' ? 'text' : 'password'"
-          tabindex="0"
-        />
+        <Icon v-if="rightIcon || props.type == 'password'" :class="[
+          'h-4 aspect-square cursor-pointer text-primary InputRightIcon',
+          classes?.rightIcon
+        ]" :path="rightIcon ?? type == 'password' ? mdiEyeOutline : mdiEyeOffOutline
+  " @click="type = type == 'password' ? 'text' : 'password'" tabindex="0" />
       </slot>
     </div>
-    <span
-      v-if="error"
-      :class="['text-sm font-bold text-red-600 Input-Icon']"
-      v-text="error"
-    />
+    <span v-if="error" :class="['text-sm font-bold text-red-600 Input-Icon']" v-text="error" />
   </div>
 </template>
 
