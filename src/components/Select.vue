3839d51dfbc1bbf1473fe9305cd4e574c57ea441
Minister <mundestephane13@gmail.com>
2023-06-24 17:35:23 +0100

    Some additional setup

--- src/components/Select.vue
+++ src/components/Select.vue
@@ -1,30 +1,31 @@
 <template>
   <div class="Select">
-    <div class="Select-Wrapper">
+    <div
+      :class="['Select-Wrapper', classes?.wrapper, { 'p-1': size == 'sm', 'p-2': size == 'md', 'p-3': size == 'lg' }]">
       <slot name="leftIcon">
-        <Icon v-if="leftIcon" :class="['Select-LeftIcon Select-Icon', classes.icon, classes?.leftIcon]" :path="leftIcon" />
+        <Icon v-if="leftIcon" :class="['Select-LeftIcon Select-Icon', classes?.icon, classes?.leftIcon]"
+          :path="leftIcon" />
       </slot>
 
       <div class="grow">
         <select :placeholder="placeholder" :class="[classes?.select,
-          'focus:outline-none h-full w-full bg-transparent SelectInut',
+          'Select-Input',
         ]" :required="required" @input="
-    $emit('update:model-value', ($event.target as HTMLInputElement).value)
-    ">
+  $emit('update:model-value', ($event.target as HTMLInputElement).value)
+  ">
           <option v-for="option in options" :value="option.value" :key="`${option.label}-${option.value}`"
-            :class="[classes?.option, 'Select-Option']">
+            :class="[classes?.option, 'Select-Option text-red-500']">
             {{ option.label }}
           </option>
         </select>
-      </div>  
+      </div>
 
       <slot name="rightIcon">
         <Icon v-if="rightIcon" :class="[
           'SelectIcon Select-RightIcon',
           classes?.icon,
           classes?.rightIcon,
-        ]" :path="rightIcon ?? type == 'password' ? mdiEyeOutline : mdiEyeOffOutline
-  " @click="type = type == 'password' ? 'text' : 'password'" tabindex="0" />
+        ]" :path="rightIcon" tabindex="0" />
       </slot>
     </div>
     <span v-if="error" :class="['text-sm font-bold text-red-600']" v-text="error" />
@@ -39,12 +40,14 @@ interface InputProps {
   leftIcon?: string
   rightIcon?: string
   placeholder?: string
-  modelValue: string | number | null
-  required: boolean
+  modelValue?: string | number | null
+  required?: boolean
   error?: string | null
   options: { label: string; value?: string | null }[]
+  size?: 'sm' | 'md' | 'lg'
   classes?: {
-    icon?: string 
+    wrapper?: string
+    icon?: string
     leftIcon?: string
     select?: string
     rightIcon?: string
@@ -53,7 +56,9 @@ interface InputProps {
   }
 }
 
-withDefaults(defineProps<InputProps>(), {})
+withDefaults(defineProps<InputProps>(), {
+  size: 'md'
+})
 </script>
 
 <style scoped></style>
